c615539d0fa16d0fdd7304ab4aa8c9d482a3bb53
Hasan Can <aslan4044@gmail.com>
2021-01-06 23:59:23 +0300

    fix: change file names

--- dataloader/threedmatch_loader.py
+++ dataloader/threedmatch_loader.py
@@ -39,7 +39,7 @@ class IndoorPairDataset(PairDataset):
 
         subset_names = open(self.DATA_FILES[phase]).read().split()
         for name in subset_names:
-            fname = name + "@seq-01.txt"
+            fname = name + ".txt"
             fnames_txt = glob.glob(root + "/" + fname)
             assert (
                 len(fnames_txt) > 0
