36e1cca8eb4708f2f083a7a8d9d862e1a952a685
Hasan Can <aslan4044@gmail.com>
2021-01-06 23:48:59 +0300

    feat: change dataloader to get data

--- dataloader/threedmatch_loader.py
+++ dataloader/threedmatch_loader.py
@@ -125,7 +125,7 @@ class IndoorPairDataset(PairDataset):
 
 
 class ThreeDMatchPairDataset03(IndoorPairDataset):
-  OVERLAP_RATIO = 0.3
+  OVERLAP_RATIO = None
   DATA_FILES = {
       'train': './dataloader/split/train_3dmatch.txt',
       'val': './dataloader/split/val_3dmatch.txt',
