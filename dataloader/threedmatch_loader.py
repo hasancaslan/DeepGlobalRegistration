2b0c8b39daf1baf43baf25774f64a8b9d9e71b61
Hasan Can <aslan4044@gmail.com>
2021-01-06 23:55:04 +0300

    fix: change txt file names

--- dataloader/threedmatch_loader.py
+++ dataloader/threedmatch_loader.py
@@ -14,183 +14,200 @@ from util.file import read_trajectory
 
 
 class IndoorPairDataset(PairDataset):
-  '''
-  Train dataset
-  '''
-  OVERLAP_RATIO = None
-  AUGMENT = None
+    """
+    Train dataset
+    """
 
-  def __init__(self,
-               phase,
-               transform=None,
-               random_rotation=True,
-               random_scale=True,
-               manual_seed=False,
-               config=None):
-    PairDataset.__init__(self, phase, transform, random_rotation, random_scale,
-                         manual_seed, config)
-    self.root = root = config.threed_match_dir
-    self.use_xyz_feature = config.use_xyz_feature
-    logging.info(f"Loading the subset {phase} from {root}")
+    OVERLAP_RATIO = None
+    AUGMENT = None
 
-    subset_names = open(self.DATA_FILES[phase]).read().split()
-    for name in subset_names:
-      fname = name + "@seq-01-%.2f.txt" % self.OVERLAP_RATIO
-      fnames_txt = glob.glob(root + "/" + fname)
-      assert len(fnames_txt) > 0, f"Make sure that the path {root} has data {fname}"
-      for fname_txt in fnames_txt:
-        with open(fname_txt) as f:
-          content = f.readlines()
-        fnames = [x.strip().split() for x in content]
-        for fname in fnames:
-          self.files.append([fname[0], fname[1]])
+    def __init__(
+        self,
+        phase,
+        transform=None,
+        random_rotation=True,
+        random_scale=True,
+        manual_seed=False,
+        config=None,
+    ):
+        PairDataset.__init__(
+            self, phase, transform, random_rotation, random_scale, manual_seed, config
+        )
+        self.root = root = config.threed_match_dir
+        self.use_xyz_feature = config.use_xyz_feature
+        logging.info(f"Loading the subset {phase} from {root}")
 
-  def __getitem__(self, idx):
-    file0 = os.path.join(self.root, self.files[idx][0])
-    file1 = os.path.join(self.root, self.files[idx][1])
-    data0 = np.load(file0)
-    data1 = np.load(file1)
-    xyz0 = data0["pcd"]
-    xyz1 = data1["pcd"]
-    matching_search_voxel_size = self.matching_search_voxel_size
+        subset_names = open(self.DATA_FILES[phase]).read().split()
+        for name in subset_names:
+            fname = name + "@seq-01.txt"
+            fnames_txt = glob.glob(root + "/" + fname)
+            assert (
+                len(fnames_txt) > 0
+            ), f"Make sure that the path {root} has data {fname}"
+            for fname_txt in fnames_txt:
+                with open(fname_txt) as f:
+                    content = f.readlines()
+                fnames = [x.strip().split() for x in content]
+                for fname in fnames:
+                    self.files.append([fname[0], fname[1]])
 
-    if self.random_scale and random.random() < 0.95:
-      scale = self.min_scale + \
-          (self.max_scale - self.min_scale) * random.random()
-      matching_search_voxel_size *= scale
-      xyz0 = scale * xyz0
-      xyz1 = scale * xyz1
+    def __getitem__(self, idx):
+        file0 = os.path.join(self.root, self.files[idx][0])
+        file1 = os.path.join(self.root, self.files[idx][1])
+        data0 = np.load(file0)
+        data1 = np.load(file1)
+        xyz0 = data0["pcd"]
+        xyz1 = data1["pcd"]
+        matching_search_voxel_size = self.matching_search_voxel_size
 
-    if self.random_rotation:
-      T0 = sample_random_trans(xyz0, self.randg, self.rotation_range)
-      T1 = sample_random_trans(xyz1, self.randg, self.rotation_range)
-      trans = T1 @ np.linalg.inv(T0)
+        if self.random_scale and random.random() < 0.95:
+            scale = self.min_scale + (self.max_scale - self.min_scale) * random.random()
+            matching_search_voxel_size *= scale
+            xyz0 = scale * xyz0
+            xyz1 = scale * xyz1
 
-      xyz0 = self.apply_transform(xyz0, T0)
-      xyz1 = self.apply_transform(xyz1, T1)
-    else:
-      trans = np.identity(4)
+        if self.random_rotation:
+            T0 = sample_random_trans(xyz0, self.randg, self.rotation_range)
+            T1 = sample_random_trans(xyz1, self.randg, self.rotation_range)
+            trans = T1 @ np.linalg.inv(T0)
 
-    # Voxelization
-    xyz0_th = torch.from_numpy(xyz0)
-    xyz1_th = torch.from_numpy(xyz1)
+            xyz0 = self.apply_transform(xyz0, T0)
+            xyz1 = self.apply_transform(xyz1, T1)
+        else:
+            trans = np.identity(4)
 
-    sel0 = ME.utils.sparse_quantize(xyz0_th / self.voxel_size, return_index=True)
-    sel1 = ME.utils.sparse_quantize(xyz1_th / self.voxel_size, return_index=True)
+        # Voxelization
+        xyz0_th = torch.from_numpy(xyz0)
+        xyz1_th = torch.from_numpy(xyz1)
 
-    # Make point clouds using voxelized points
-    pcd0 = make_open3d_point_cloud(xyz0[sel0])
-    pcd1 = make_open3d_point_cloud(xyz1[sel1])
+        sel0 = ME.utils.sparse_quantize(xyz0_th / self.voxel_size, return_index=True)
+        sel1 = ME.utils.sparse_quantize(xyz1_th / self.voxel_size, return_index=True)
 
-    # Select features and points using the returned voxelized indices
-    # 3DMatch color is not helpful
-    # pcd0.colors = o3d.utility.Vector3dVector(color0[sel0])
-    # pcd1.colors = o3d.utility.Vector3dVector(color1[sel1])
+        # Make point clouds using voxelized points
+        pcd0 = make_open3d_point_cloud(xyz0[sel0])
+        pcd1 = make_open3d_point_cloud(xyz1[sel1])
 
-    # Get matches
-    matches = get_matching_indices(pcd0, pcd1, trans, matching_search_voxel_size)
+        # Select features and points using the returned voxelized indices
+        # 3DMatch color is not helpful
+        # pcd0.colors = o3d.utility.Vector3dVector(color0[sel0])
+        # pcd1.colors = o3d.utility.Vector3dVector(color1[sel1])
 
-    # Get features
-    npts0 = len(sel0)
-    npts1 = len(sel1)
+        # Get matches
+        matches = get_matching_indices(pcd0, pcd1, trans, matching_search_voxel_size)
 
-    feats_train0, feats_train1 = [], []
+        # Get features
+        npts0 = len(sel0)
+        npts1 = len(sel1)
 
-    unique_xyz0_th = xyz0_th[sel0]
-    unique_xyz1_th = xyz1_th[sel1]
+        feats_train0, feats_train1 = [], []
 
-    # xyz as feats
-    if self.use_xyz_feature:
-      feats_train0.append(unique_xyz0_th - unique_xyz0_th.mean(0))
-      feats_train1.append(unique_xyz1_th - unique_xyz1_th.mean(0))
-    else:
-      feats_train0.append(torch.ones((npts0, 1)))
-      feats_train1.append(torch.ones((npts1, 1)))
+        unique_xyz0_th = xyz0_th[sel0]
+        unique_xyz1_th = xyz1_th[sel1]
 
-    feats0 = torch.cat(feats_train0, 1)
-    feats1 = torch.cat(feats_train1, 1)
+        # xyz as feats
+        if self.use_xyz_feature:
+            feats_train0.append(unique_xyz0_th - unique_xyz0_th.mean(0))
+            feats_train1.append(unique_xyz1_th - unique_xyz1_th.mean(0))
+        else:
+            feats_train0.append(torch.ones((npts0, 1)))
+            feats_train1.append(torch.ones((npts1, 1)))
 
-    coords0 = torch.floor(unique_xyz0_th / self.voxel_size)
-    coords1 = torch.floor(unique_xyz1_th / self.voxel_size)
+        feats0 = torch.cat(feats_train0, 1)
+        feats1 = torch.cat(feats_train1, 1)
 
-    if self.transform:
-      coords0, feats0 = self.transform(coords0, feats0)
-      coords1, feats1 = self.transform(coords1, feats1)
+        coords0 = torch.floor(unique_xyz0_th / self.voxel_size)
+        coords1 = torch.floor(unique_xyz1_th / self.voxel_size)
 
-    extra_package = {'idx': idx, 'file0': file0, 'file1': file1}
+        if self.transform:
+            coords0, feats0 = self.transform(coords0, feats0)
+            coords1, feats1 = self.transform(coords1, feats1)
 
-    return (unique_xyz0_th.float(),
-            unique_xyz1_th.float(), coords0.int(), coords1.int(), feats0.float(),
-            feats1.float(), matches, trans, extra_package)
+        extra_package = {"idx": idx, "file0": file0, "file1": file1}
+
+        return (
+            unique_xyz0_th.float(),
+            unique_xyz1_th.float(),
+            coords0.int(),
+            coords1.int(),
+            feats0.float(),
+            feats1.float(),
+            matches,
+            trans,
+            extra_package,
+        )
 
 
 class ThreeDMatchPairDataset03(IndoorPairDataset):
-  OVERLAP_RATIO = None
-  DATA_FILES = {
-      'train': './dataloader/split/train_3dmatch.txt',
-      'val': './dataloader/split/val_3dmatch.txt',
-      'test': './dataloader/split/test_3dmatch.txt'
-  }
+    OVERLAP_RATIO = None
+    DATA_FILES = {
+        "train": "./dataloader/split/train_3dmatch.txt",
+        "val": "./dataloader/split/val_3dmatch.txt",
+        "test": "./dataloader/split/test_3dmatch.txt",
+    }
 
 
 class ThreeDMatchPairDataset05(ThreeDMatchPairDataset03):
-  OVERLAP_RATIO = 0.5
+    OVERLAP_RATIO = 0.5
 
 
 class ThreeDMatchPairDataset07(ThreeDMatchPairDataset03):
-  OVERLAP_RATIO = 0.7
+    OVERLAP_RATIO = 0.7
 
 
 class ThreeDMatchTrajectoryDataset(PairDataset):
-  '''
-  Test dataset
-  '''
-  DATA_FILES = {
-      'train': './dataloader/split/train_3dmatch.txt',
-      'val': './dataloader/split/val_3dmatch.txt',
-      'test': './dataloader/split/test_3dmatch.txt'
-  }
+    """
+    Test dataset
+    """
 
-  def __init__(self,
-               phase,
-               transform=None,
-               random_rotation=True,
-               random_scale=True,
-               manual_seed=False,
-               scene_id=None,
-               config=None,
-               return_ply_names=False):
+    DATA_FILES = {
+        "train": "./dataloader/split/train_3dmatch.txt",
+        "val": "./dataloader/split/val_3dmatch.txt",
+        "test": "./dataloader/split/test_3dmatch.txt",
+    }
 
-    PairDataset.__init__(self, phase, transform, random_rotation, random_scale,
-                         manual_seed, config)
+    def __init__(
+        self,
+        phase,
+        transform=None,
+        random_rotation=True,
+        random_scale=True,
+        manual_seed=False,
+        scene_id=None,
+        config=None,
+        return_ply_names=False,
+    ):
 
-    self.root = config.threed_match_dir
+        PairDataset.__init__(
+            self, phase, transform, random_rotation, random_scale, manual_seed, config
+        )
 
-    subset_names = open(self.DATA_FILES[phase]).read().split()
-    if scene_id is not None:
-      subset_names = [subset_names[scene_id]]
-    for sname in subset_names:
-      traj_file = os.path.join(self.root, sname + '-evaluation/gt.log')
-      assert os.path.exists(traj_file)
-      traj = read_trajectory(traj_file)
-      for ctraj in traj:
-        i = ctraj.metadata[0]
-        j = ctraj.metadata[1]
-        T_gt = ctraj.pose
-        self.files.append((sname, i, j, T_gt))
+        self.root = config.threed_match_dir
 
-    self.return_ply_names = return_ply_names
+        subset_names = open(self.DATA_FILES[phase]).read().split()
+        if scene_id is not None:
+            subset_names = [subset_names[scene_id]]
+        for sname in subset_names:
+            traj_file = os.path.join(self.root, sname + "-evaluation/gt.log")
+            assert os.path.exists(traj_file)
+            traj = read_trajectory(traj_file)
+            for ctraj in traj:
+                i = ctraj.metadata[0]
+                j = ctraj.metadata[1]
+                T_gt = ctraj.pose
+                self.files.append((sname, i, j, T_gt))
 
-  def __getitem__(self, pair_index):
-    sname, i, j, T_gt = self.files[pair_index]
-    ply_name0 = os.path.join(self.root, sname, f'cloud_bin_{i}.ply')
-    ply_name1 = os.path.join(self.root, sname, f'cloud_bin_{j}.ply')
+        self.return_ply_names = return_ply_names
 
-    if self.return_ply_names:
-      return sname, ply_name0, ply_name1, T_gt
+    def __getitem__(self, pair_index):
+        sname, i, j, T_gt = self.files[pair_index]
+        ply_name0 = os.path.join(self.root, sname, f"cloud_bin_{i}.ply")
+        ply_name1 = os.path.join(self.root, sname, f"cloud_bin_{j}.ply")
 
-    pcd0 = o3d.io.read_point_cloud(ply_name0)
-    pcd1 = o3d.io.read_point_cloud(ply_name1)
-    pcd0 = np.asarray(pcd0.points)
-    pcd1 = np.asarray(pcd1.points)
-    return sname, pcd0, pcd1, T_gt
+        if self.return_ply_names:
+            return sname, ply_name0, ply_name1, T_gt
+
+        pcd0 = o3d.io.read_point_cloud(ply_name0)
+        pcd1 = o3d.io.read_point_cloud(ply_name1)
+        pcd0 = np.asarray(pcd0.points)
+        pcd1 = np.asarray(pcd1.points)
+        return sname, pcd0, pcd1, T_gt
