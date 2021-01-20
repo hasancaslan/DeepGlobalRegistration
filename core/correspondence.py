0a92b4356b7bb8e314a1eb7815895be02ae8ae42
Hasan Can <aslan4044@gmail.com>
2021-01-20 20:49:18 +0300

    chore: add breakpoints

--- core/correspondence.py
+++ core/correspondence.py
@@ -12,43 +12,43 @@ import torch
 
 
 def _hash(arr, M=None):
-  if isinstance(arr, np.ndarray):
-    N, D = arr.shape
-  else:
-    N, D = len(arr[0]), len(arr)
-
-  hash_vec = np.zeros(N, dtype=np.int64)
-  for d in range(D):
     if isinstance(arr, np.ndarray):
-      hash_vec += arr[:, d] * M**d
+        N, D = arr.shape
     else:
-      hash_vec += arr[d] * M**d
-  return hash_vec
+        N, D = len(arr[0]), len(arr)
+
+    hash_vec = np.zeros(N, dtype=np.int64)
+    for d in range(D):
+        if isinstance(arr, np.ndarray):
+            hash_vec += arr[:, d] * M ** d
+        else:
+            hash_vec += arr[d] * M ** d
+    return hash_vec
 
 
 def find_correct_correspondence(pos_pairs, pred_pairs, hash_seed=None, len_batch=None):
-  assert len(pos_pairs) == len(pred_pairs)
-  if hash_seed is None:
-    assert len(len_batch) == len(pos_pairs)
-
-  corrects = []
-  for i, pos_pred in enumerate(zip(pos_pairs, pred_pairs)):
-    pos_pair, pred_pair = pos_pred
-    if isinstance(pos_pair, torch.Tensor):
-      pos_pair = pos_pair.numpy()
-    if isinstance(pred_pair, torch.Tensor):
-      pred_pair = pred_pair.numpy()
-
+    assert len(pos_pairs) == len(pred_pairs)
     if hash_seed is None:
-      N0, N1 = len_batch[i]
-      _hash_seed = max(N0, N1)
-    else:
-      _hash_seed = hash_seed
+        assert len(len_batch) == len(pos_pairs)
 
-    pos_keys = _hash(pos_pair, _hash_seed)
-    pred_keys = _hash(pred_pair, _hash_seed)
+    corrects = []
+    for i, pos_pred in enumerate(zip(pos_pairs, pred_pairs)):
+        pos_pair, pred_pair = pos_pred
+        if isinstance(pos_pair, torch.Tensor):
+            pos_pair = pos_pair.numpy()
+        if isinstance(pred_pair, torch.Tensor):
+            pred_pair = pred_pair.numpy()
 
-    corrects.append(np.isin(pred_keys, pos_keys, assume_unique=False))
+        if hash_seed is None:
+            N0, N1 = len_batch[i]
+            _hash_seed = max(N0, N1)
+        else:
+            _hash_seed = hash_seed
 
-  return np.hstack(corrects)
+        pos_keys = _hash(pos_pair, _hash_seed)
+        pred_keys = _hash(pred_pair, _hash_seed)
 
+        corrects.append(np.isin(pred_keys, pos_keys, assume_unique=False))
+
+    breakpoint()
+    return np.hstack(corrects)
